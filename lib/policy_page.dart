8b2e48278d6155d46d6a4ed6bb05f279884442ec
shekkanen <sami.hekkanen@gmail.com>
2024-09-21 11:51:34 +0300

    policy page

---
+++ lib/policy_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class PolicyPage extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const PolicyPage({Key? key, required this.title, required this.content})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.black, // Match your app's theme
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          content,
+          style: const TextStyle(fontSize: 16.0),
+        ),
+        // Implement text selection
+      ),
+    );
+  }
+}
