cc78993ec4a294f15fdd5f4fa91faf45c913c970
shekkanen <sami.hekkanen@gmail.com>
2024-10-26 11:35:37 +0300

    Add debug and release build scripts for installing APKs

---
+++ debug_build_and_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#flutter clean
+
+#flutter pub get
+
+# Build debug APK
+if flutter build apk --debug; then
+    echo "Build succeeded, installing APK..."
+
+  # Install debug APK to my Xiaomi phone
+    flutter install --debug -d e7d36532
+    #flutter install --upgrade --debug -d e7d36532
+
+  # Install debug APK to my Redmi 8 phone
+    flutter install --debug -d 18a3a90b9907
+    #flutter install --upgrade --debug -d 18a3a90b9907
+
+else
+  echo "Build failed, not installing APK."
+fi
---
+++ release_build_and_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#flutter clean
+
+#flutter pub get
+
+# Build release APK
+if flutter build apk --release; then
+    echo "Build succeeded, installing APK..."
+
+  # Install release APK to my Xiaomi phone
+    flutter install --release -d e7d36532
+    #flutter install --upgrade --release -d e7d36532
+
+  # Install release APK to my Redmi 8 phone
+    flutter install --release -d 18a3a90b9907
+    #flutter install --upgrade --release -d 18a3a90b9907
+
+else
+  echo "Build failed, not installing APK."
+fi
